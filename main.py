32c4be6cd5c0eac778212f38cb23d994f08f59f6
Alireza <alirezanick66@gmail.com>
2024-10-20 11:52:14 +0330

    یکم شکل کد هارو بهتر کردم

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ lletters = [ 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , '
 numbers = [ '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' ]
 symbols = [ '!' , '@' , '#' , '$' , '%' , '^' , '&' , '*' , '(' , ')' , '-' , '_' , '=' , '+' , '[' , ']' , '{' , '}' , ';' , ':' , '<' , '>' , ',' , '.' , '/' , '?' , '|' , '~' , '`' ]
 
-# کپشن های نمایش
+# تایتل های نمایش
 welcome_title = "hi , You can create strong password !"
 pass_count_title = "How many passwords? :  "
 numbers_count_title = "How Many  numbers?"
 symbole_count_title = "How Many symbols?"
 
-# پردازش
+# مرحله نمایش
 print ( welcome_title.title ( ) )
 pass_count = int ( input ( pass_count_title ) )
 numbers_Count = int ( input ( numbers_count_title.title ( ) ) )
 symbole_count = int ( input ( symbole_count_title.title ( ) ) )
 
-# خروجی
+#  مرحله پردازش
 password_list = [ ]
-
 for char in range ( 0 , pass_count ) :
     password_list.append ( random.choice ( lletters ) )
 
@@ -29,5 +28,7 @@ for char in range ( 0 , numbers_Count ) :
 for char in range ( 0 , symbole_count ) :
     password_list.append ( random.choice ( symbols ) )
 
+# خروجی
 random.shuffle ( password_list )
 print ( password_list )
+
