f5da617c57ad9dbf669064642332f25678a8e48b
Alireza <alirezanick66@gmail.com>
2024-10-20 11:47:36 +0330

    اولین پروژه ام تموم شد

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import random
 
 # داده های اولیه
-lowercase_letters = [ 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 'pass_count' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' ]
-numbers = [ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ]
+lletters = [ 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 'pass_count' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' ]
+numbers = [ '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' ]
 symbols = [ '!' , '@' , '#' , '$' , '%' , '^' , '&' , '*' , '(' , ')' , '-' , '_' , '=' , '+' , '[' , ']' , '{' , '}' , ';' , ':' , '<' , '>' , ',' , '.' , '/' , '?' , '|' , '~' , '`' ]
 
 # کپشن های نمایش
 welcome_title = "hi , You can create strong password !"
 pass_count_title = "How many passwords? :  "
-include_numbers_title = "Does it include numbers?"
-include_symbole_title = "Does it include symbols?"
+numbers_count_title = "How Many  numbers?"
+symbole_count_title = "How Many symbols?"
 
 # پردازش
 print ( welcome_title.title ( ) )
-pass_count = input ( pass_count_title )
-is_need_number = input ( include_numbers_title.title ( ) )
-is_need_symbole = input ( include_symbole_title.title ( ) )
+pass_count = int ( input ( pass_count_title ) )
+numbers_Count = int ( input ( numbers_count_title.title ( ) ) )
+symbole_count = int ( input ( symbole_count_title.title ( ) ) )
 
 # خروجی
-if is_need_number == 'yes' :
-    print ( "yes" )
-else :
-    print ( "no" )
+password_list = [ ]
+
+for char in range ( 0 , pass_count ) :
+    password_list.append ( random.choice ( lletters ) )
+
+for char in range ( 0 , numbers_Count ) :
+    password_list.append ( random.choice ( numbers ) )
+
+for char in range ( 0 , symbole_count ) :
+    password_list.append ( random.choice ( symbols ) )
+
+random.shuffle ( password_list )
+print ( password_list )
